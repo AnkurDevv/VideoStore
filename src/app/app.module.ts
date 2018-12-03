e54ec8b2a679d3d002efb5ac0a7ee92fb7db4d91
Evo123456 <34413640+Evo123456@users.noreply.github.com>
2018-12-02 23:44:48 -0500

    Layout-Complete :) one minor bug with  Sign-In pg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { UserVideoListComponent } from './user-video-list/user-video-list.component';
 import { AdminVideoListComponent } from './admin-video-list/admin-video-list.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { UpdateVideoComponent } from './update-video/update-video.component';
+import { ReserveVideoComponent } from './reserve-video/reserve-video.component';
+import { AddVideoComponent } from './add-video/add-video.component';
 
 // const routes: Routes = [
 //  // { path: 'userVideoList', component: [UserVideoListComponent] },
@@ -16,7 +21,12 @@ import { AdminVideoListComponent } from './admin-video-list/admin-video-list.com
   declarations: [
     AppComponent,
     UserVideoListComponent,
-    AdminVideoListComponent
+    AdminVideoListComponent,
+    AdminLoginComponent,
+    CustomerListComponent,
+    UpdateVideoComponent,
+    ReserveVideoComponent,
+    AddVideoComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +34,12 @@ import { AdminVideoListComponent } from './admin-video-list/admin-video-list.com
     RouterModule.forRoot(
       [
         {path:'user-video-list',component: UserVideoListComponent},
-        {path:'admin-video-list',component: AdminVideoListComponent}  
+        {path:'admin-video-list',component: AdminVideoListComponent},
+        {path:'admin-login',component: AdminLoginComponent},
+        {path:'customer-list',component: CustomerListComponent},
+        {path:'add-video',component: AddVideoComponent},
+        {path:'reserve-video',component: ReserveVideoComponent}, 
+        {path:'update-video',component: UpdateVideoComponent}  
       ]
     )
   ],
